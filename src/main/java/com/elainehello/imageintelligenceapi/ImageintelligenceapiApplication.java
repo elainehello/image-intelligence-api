7a33f5013fd175f0abf2ebf6a64698aff249cf1e
elainehello <195331169+elainehello@users.noreply.github.com>
2026-05-20 11:15:27 +0200

    Initial commit

---
+++ src/main/java/com/elainehello/imageintelligenceapi/ImageintelligenceapiApplication.java
@@ -0,0 +1,13 @@
+package com.elainehello.imageintelligenceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageintelligenceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageintelligenceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elainehello/imageintelligenceapi/ImageintelligenceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elainehello.imageintelligenceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageintelligenceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
